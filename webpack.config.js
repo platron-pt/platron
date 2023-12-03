356d8d846c321d791ac58d265735f17fe534ac59
ryantsui1109 <beigua87@gmail.com>
2023-12-03 16:25:00 +0800

    add reactjs to dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,18 @@ module.exports = {
     filename: "bundle.js",
     library: "lib",
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
 };
