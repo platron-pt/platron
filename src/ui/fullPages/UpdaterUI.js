abf08862adae6aefb774523555c9d125a12b16cf
ryantsui1109 <beigua87@gmail.com>
2024-02-10 00:24:56 +0800

    show updater

--- src/ui/fullPages/UpdaterUI.js
+++ src/ui/fullPages/UpdaterUI.js
@@ -1,7 +1,58 @@
-import React from "react";
+import classNames from "classnames";
+import React, { useState } from "react";
 
-function UpdaterUI(){
-  return<></>
+let appSettings = require("../../../config.json");
+let theme = appSettings.theme;
+let appLanguage = appSettings.language;
+if (appSettings.language === "auto") {
+  switch (navigator.language) {
+    case "zh-TW":
+    case "en-US":
+      appLanguage = navigator.language;
+      break;
+    default:
+      appLanguage = "en-US";
+  }
 }
 
-export default UpdaterUI
+const messages = require("../../../res/json/lang/" +
+  appLanguage +
+  "/messages.json");
+
+function Updater(props) {
+  console.log(messages);
+  const [checkSent,setCheckSent]=useState(false)
+  return (
+    <div className="card">
+      <div className="card-body">
+        <div className="d-flex">
+          <div className="spinner-border spinner-border-sm" role="status">
+            <span className="visually-hidden">{messages.update.checking}</span>
+          </div>
+          <h6 className="align-self-center mb-0">{messages.update.checking}</h6>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function UpdaterUI() {
+  const [showUpdater, setShowUpdater] = useState(false);
+  function handleClick(e) {
+    setShowUpdater(true);
+  }
+
+  return (
+    <>
+      <button
+        className={classNames("btn", "btn-info", "mb-2")}
+        onClick={handleClick}
+      >
+        {messages.update.updateBtn}
+      </button>
+      {showUpdater ? <Updater /> : null}
+    </>
+  );
+}
+
+export default UpdaterUI;
